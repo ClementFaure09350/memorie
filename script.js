7ada35c56c4d726a90bafcbb2d9e97db7e32ea1f
ES_PREPA_DEV <Clement.faure09350@gmail.com>
2025-10-20 09:10:53 +0200

    Preparation du plateau de jeu en attente du JS

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('Ça marche !');
+/*
+<div class="card">
+                <!-- div.card-back+div.card-img -->
+                <div class="card-back"></div>
+                <div class="card-img" style="background-image:url('img/8.webp')"></div>
+            </div>
+*/
